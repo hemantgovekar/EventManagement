2a8a8d7e3868b833ae6b393833b9c75d8b61c7a7
hemantgovekar <hemantgovekar@gmail.com>
2016-12-31 11:15:21 -0600

    Add project files.

---
+++ UserWeb/Scripts/script.js
@@ -0,0 +1,55 @@
+﻿var customersApp = angular.module('customersApp', ['ngGrid']);
+customersApp.controller('customerCtrl', function ($scope, userRepository) { });
+var url = 'http://localhost:24861/api/values';
+//app.factory('MyFactory', function () {
+//    return {
+//        testfunction: function (text) {
+//            return "Hello" + text;
+//        }
+//    }
+//});
+customersApp.factory('customerRepository', function ($http) {
+    return {
+        getCustomers: function (callback) {
+           // alert(callback);
+            $http.get(url).success(callback);
+        }
+        ,
+        //method for insert
+        insertUser: function (callback, user) {
+            // var user = { "id": user.id, "city": user.city, "name": user.name, "address": user.address, "contactNo": user.contactNo, "emailId": user.emailId };
+            var user = { "UserId": user.UserId, "Username": user.Username, "Password": user.Password }
+            $http.post(url, user).success(callback);
+        }
+            ,
+        //method for update
+        updateUser: function (callback, user) {
+            var user = { "id": user.id, "city": user.city, "name": user.name, "address": user.address, "contactNo": user.contactNo, "emailId": user.emailId };
+            $http.put(url + '/' + user.id, user).success(callback);
+        }
+        ,
+        //method for delete
+        deleteUser: function (callback, id) {
+            $http.delete(url + '/' + id).success(callback);
+        }
+
+
+    }
+});
+
+customersApp.controller('customerCtrl', function ($scope, customerRepository) {
+    getCustomers();
+    function getCustomers() {
+        customerRepository.getCustomers(function (results) {
+            $scope.customerData = results;
+            
+        })
+    }
+    $scope.update = function () {
+        customerRepository.insertUser(function () {
+            alert('customer inserted successfully');
+            getCustomers();
+        }, $scope.New)
+    };
+
+});
---
+++ WebApiWithEntity/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserEntity;
+using System.Web.Http.Cors;
+
+namespace WebApiWithEntity.Controllers
+{
+    //[Authorize]
+   
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<User> Get()
+        {
+            using (TestEntities te = new TestEntities())
+            {
+                return te.Users.ToList();
+            };
+
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]User user)
+        {
+            using (TestEntities te = new TestEntities())
+            {
+                te.Users.Add(user);
+                te.SaveChanges();
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
